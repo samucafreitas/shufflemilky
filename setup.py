016eef2f15b0b5a5cbeb14fbd4f8a14c3538c995
Samuel Freitas <samucaof@hotmail.com>
2018-11-25 20:54:40 -0200

    Changed version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='shufflemilky',
-    version='0.1.0',
+    version='0.3.0',
     author='Samuel Freitas',
     author_email='samuelfreitas@linuxmail.org',
     packages=find_packages(exclude=['tests', '__pycache__']),
     platforms='unix',
     url='http://pypi.python.org/pypi/shufflemilky/',
     license='MIT',
-    description='A simple shuffle player wiritten in python.',
+    description='A simple shuffle player written in Python.',
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': ['shufflemilky = shufflemilky.shufflemilky:main']
