cd6af9daa4d1c4530e03716eebfa90560bb4392e
Samuel Freitas <samucaof@hotmail.com>
2018-11-25 21:13:04 -0200

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shufflemilky',
-    version='0.3.0',
+    version='0.4.0',
     author='Samuel Freitas',
     author_email='samuelfreitas@linuxmail.org',
     packages=find_packages(exclude=['tests', '__pycache__']),
